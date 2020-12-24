f28104b4351211e54a7e4ee46ddaffab16e82672
ladangser <76545084+ladangser@users.noreply.github.com>
2020-12-24 15:36:59 +0900

    Create katakata.js

---
+++ katakata.js
@@ -0,0 +1,16 @@
+const quotes = [
+	{isi: 'Ini adalah jalan ninjaku',
+	 pemilik: 'Nartoh'},
+	
+	{isi: 'Berjalanlah dan terus tertawa',
+	 pemilik: 'Nidji'}
+];
+
+
+const ql = quotes.length; // mengambil jumlah index pada array quotes
+const x = Math.random() * ql; // membuat angka acak dengan max sesuai panjang array quotes
+const random_num = Math.floor(x); // membulatkan angka kebawah
+
+function katakata() {
+	console.log(quotes[random_num]); // menampilkan hasil
+}
