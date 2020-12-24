b5c39cc9772e7bc850d02c6ed9cb1f82fc29fbab
ladangser <76545084+ladangser@users.noreply.github.com>
2020-12-24 16:07:38 +0900

    menambahkan beberapa quotes

--- katakata.js
+++ katakata.js
@@ -1,11 +1,29 @@
 console.log('--Ladangser Quotes--');
 
+// Kumpulan Quotes
 const quotes = [
-	{isi: 'Ini adalah jalan ninjaku',
-	 pemilik: 'Nartoh'},
 	
-	{isi: 'Berjalanlah dan terus tertawa',
-	 pemilik: 'Nidji'}
+	{isi: 'Berjalanlah dan terus tertawa.',
+	 pemilik: 'Nidji'},
+	
+	{isi: 'Sabar itu nggak ada batasnya, kalau ada batasnya berarti nggak sabar.',
+	 pemilik: 'Gus Dur'},
+	
+	{isi: 'Memuliakan manusia berarti memuliakan penciptanya. Merendahkan manusia berarti merendahkan dan menistakan penciptanya.',
+	 pemilik: 'Gus Dur'},
+	
+	{isi: 'Tidak apa-apa untuk merayakan kesuksesan tapi lebih penting untuk memperhatikan pelajaran tentang kegagalan.',
+	 pemilik: 'Bill Gates'},
+	
+	{isi: 'Jangan takut untuk membuat sebuah kesalahan. Tapi pastikan Anda tidak melakukan kesalahan yang sama dua kali.',
+	 pemilik: 'Akio Morita'},
+	
+	{isi: 'Tidak mungkin hidup tanpa gagal dalam sesuatu, kecuali jika Anda hidup dengan sangat hati-hati sehingga Anda mungkin juga tidak hidup sama sekali – dalam hal ini, Anda gagal karena kelalaian.',
+	 pemilik: 'J. K. Rowling'},
+	
+	{isi: 'Teruslah tersenyum, karena hidup itu indah dan ada banyak hal yang bisa disyukuri.',
+	 pemilik: 'Marilyn Monroe'}
+	 
 ];
 
 
